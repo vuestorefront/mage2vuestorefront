929430a40eef9d4a7051f38a70bbcf80ce58032d
pkarwatka <pkarwatka@divante.pl>
2017-10-04 21:24:17 +0200

    Attributes sync added

--- src/adapters/magento/magento2-rest-client/index.js
+++ src/adapters/magento/magento2-rest-client/index.js
@@ -2,6 +2,7 @@
 
 var RestClient = require('./lib/rest_client').RestClient;
 var categories = require('./lib/categories');
+var attributes = require('./lib/attributes');
 var products = require('./lib/products');
 var productMedia = require('./lib/product_media');
 var categoryProducts = require('./lib/category_products');
@@ -16,6 +17,7 @@ module.exports.Magento2Client = function (options) {
     
     var client = RestClient(options);
 
+    instance.attributes = attributes(client);
     instance.categories = categories(client);
     instance.products = products(client);
     instance.productMedia = productMedia(client);
