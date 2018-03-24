718248035d75eace5696933ed64b27e65812f5ab
pkarw <pkarwatka@divante.pl>
2018-03-24 16:31:05 +0100

    Fixed #15

--- src/adapters/magento/magento2-rest-client/lib/products.js
+++ src/adapters/magento/magento2-rest-client/lib/products.js
@@ -14,12 +14,12 @@ module.exports = function (restClient) {
     }
 
     module.update = function (productSku, productAttributes) {
-        var endpointUrl = util.format('/products/%s', productSku);
+        var endpointUrl = util.format('/products/%s', encodeURIComponent(productSku));
         return restClient.put(endpointUrl, productAttributes);
     }
 
     module.delete = function (productSku) {
-        var endpointUrl = util.format('/products/%s', productSku);
+        var endpointUrl = util.format('/products/%s', encodeURIComponent(productSku));
         return restClient.delete(endpointUrl);
     }
 
