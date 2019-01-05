3c0428d04d5c79eacc2bb5db3bb8a96a1b350077
Cyclonecode <cyclonecode@gmail.com>
2019-01-05 14:47:32 +0100

    Use platform independent path to tmp folder

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@
 
 const program = require('commander');
 const fs = require('fs');
+const path = require('path');
 let AdapterFactory = require('./adapters/factory');
 
 const TIME_TO_EXIT = process.env.TIME_TO_EXIT ? process.env.TIME_TO_EXIT : 30000; // wait 30s before quiting after task is done
@@ -10,7 +11,7 @@ let config = require('./config');
 let logger = require('./log');
 let factory = new AdapterFactory(config);
 const jsonFile = require('jsonfile')
-const INDEX_META_PATH = process.env.INDEX_META_PATH ? './tmp/' + process.env.INDEX_META_PATH : './tmp/.lastIndex.json'
+const INDEX_META_PATH = process.env.INDEX_META_PATH ? path.join('tmp', process.env.INDEX_META_PATH) : path.join('tmp', '.lastIndex.json')
 
 let kue = require('kue');
 let queue = kue.createQueue(Object.assign(config.kue, { redis: config.redis }));
@@ -382,8 +383,8 @@ program
     let updatedAfter = null
     try {
       // Make sure so the temporary folder exists.
-      if (!fs.existsSync('./tmp')) {
-        fs.mkdirSync('./tmp')
+      if (!fs.existsSync('tmp')) {
+        fs.mkdirSync('tmp')
       }
       indexMeta = jsonFile.readFileSync(INDEX_META_PATH)
       updatedAfter = new Date(indexMeta.lastIndexDate)
